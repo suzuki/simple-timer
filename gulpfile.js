6050a14bbdb826e3869091f6e36efa3bda0a891b
Norio Suzuki <norio.suzuki@gmail.com>
2015-04-19 22:31:29 +0900

    Add gulp-jest task

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,9 @@ var gulp = require('gulp');
 var browserify = require('browserify');
 var source = require('vinyl-source-stream');
 var reactify = require('reactify');
+var jest = require('gulp-jest');
 
+require("harmonize")();
 
 gulp.task('browserify', function() {
   var b = browserify({
@@ -17,8 +19,28 @@ gulp.task('browserify', function() {
          .pipe(gulp.dest('./build'));
 });
 
+gulp.task('jest', function() {
+  return gulp.src('src')
+         .pipe(jest({
+           scriptPreprocessor: "../preprocessor.js",
+           unmockedModulePathPatterns: [
+             "node_modules/react"
+           ],
+           testDirectoryName: "__tests__",
+           testPathIgnorePatterns: [
+             "node_modules"
+           ],
+           moduleFileExtensions: [
+             "js",
+             "json",
+             "react"
+           ]
+         }));
+});
+
+
 gulp.task('watch', function() {
-  gulp.watch('./src/**/*.jsx', ['browserify']);
+  gulp.watch('./src/**/*.jsx', ['jest', 'browserify']);
 });
 
 gulp.task('default', ['browserify']);
