270863d9e45c468194c5078984f82038cd872e8d
Norio Suzuki <norio.suzuki@gmail.com>
2015-04-20 08:20:32 +0900

    Refactor test case.

--- src/components/__tests__/StartButton.spec.js
+++ src/components/__tests__/StartButton.spec.js
@@ -5,27 +5,35 @@
 jest.dontMock('../StartButton.jsx');
 
 var React = require('react/addons');
-var StartButton = require('../StartButton.jsx');
 var TestUtils = React.addons.TestUtils;
 var Simulate = TestUtils.Simulate;
 
+var StartButton = require('../StartButton.jsx');
+
 describe('StartButton', function() {
-  it ('start button', function() {
-    var time = 1429453037035;
-    var mockDate = {
-      getTime: jest.genMockFunction().mockReturnValue(time)
+
+  beforeEach(function() {
+    this.time = 1429453037035;
+    this.mockCallback = jest.genMockFunction();
+
+    this.mockDate = {
+      getTime: jest.genMockFunction().mockReturnValue(this.time)
     };
 
-    var mockCallback = jest.genMockFunction();
-    var startButton = TestUtils.renderIntoDocument(
-      <StartButton onStartClick={mockCallback} date={mockDate} />
+    this.startButton = TestUtils.renderIntoDocument(
+      <StartButton onStartClick={this.mockCallback} date={this.mockDate} />
     );
+  });
 
-    expect(startButton.getDOMNode().textContent).toEqual('Start');
 
-    var node = startButton.getDOMNode();
+  it ('StartButton has Start text', function() {
+    expect(this.startButton.getDOMNode().textContent).toEqual('Start');
+  });
+
+  it ('When StartButton clicked, call callback function', function() {
+    var node = this.startButton.getDOMNode();
     Simulate.click(node);
 
-    expect(mockCallback).toBeCalledWith(time);
+    expect(this.mockCallback).toBeCalledWith(this.time);
   });
 });
